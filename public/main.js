0190139e793eb92c35190bbfe5f336ce44a56946
hojin-chu <cnghwls12@naver.com>
2019-09-27 16:51:41 +0900

    realtime vote application _ Nodejs&pPusher&MongoDB

--- public/main.js
+++ public/main.js
@@ -19,48 +19,59 @@ form.addEventListener('submit', (e) => {
   e.preventDefault()
 })
 
-let dataPoints = [
-  { label: 'Windows', y: 0 },
-  { label: 'MacOS', y: 0 },
-  { label: 'Linux', y: 0 },
-  { label: 'Other', y: 0 },
-]
+fetch('http://localhost:3000/poll')
+  .then(res => res.json())
+  .then(data => {
+    const votes = data.votes
+    const totalVotes = votes.length
+    // Count vote points - acc/current
+    const voteCounts = votes.reduce((acc, vote) => ((acc[vote.os] = (acc[vote.os] || 0) + parseInt(vote.points)), acc), {})
 
-const chartContainer = document.querySelector('#chartContainer')
-
-if(chartContainer) {
-  const chart = new CanvasJS.Chart('chartContainer', {
-    animationEnabled: true,
-    exportEnabled: true,
-    theme: 'theme1',
-    title: {
-      text: 'OS Results'
-    },
-    data: [{
-      type: 'column',
-      dataPoints: dataPoints
-    }]
+    let dataPoints = [
+      { label: 'Windows', y: voteCounts.Windows },
+      { label: 'MacOS', y: voteCounts.MacOS },
+      { label: 'Linux', y: voteCounts.Linux },
+      { label: 'Other', y: voteCounts.Other },
+    ]
+    
+    const chartContainer = document.querySelector('#chartContainer')
+    
+    if(chartContainer) {
+      const chart = new CanvasJS.Chart('chartContainer', {
+        animationEnabled: true,
+        exportEnabled: true,
+        theme: 'theme1',
+        title: {
+          text: `Total Votes ${totalVotes}`
+        },
+        data: [{
+          type: 'column',
+          dataPoints: dataPoints
+        }]
+      })
+      chart.render()
+    
+      Pusher.logToConsole = true
+    
+      var pusher = new Pusher('a04876fd6f30e492290f', {
+        cluster: 'us2',
+        encrypted: true
+      })
+    
+      var channel = pusher.subscribe('os-poll')
+      channel.bind('os-vote', data => {
+        dataPoints = dataPoints.map(x => {
+          if (x.label == data.os) {
+            x.y += data.points
+            return x
+          } else {
+            return x
+          }
+        })
+        chart.render()
+      })
+    }
   })
-  chart.render()
 
-  Pusher.logToConsole = true
 
-  var pusher = new Pusher('a04876fd6f30e492290f', {
-    cluster: 'us2',
-    encrypted: true
-  })
-
-  var channel = pusher.subscribe('os-poll')
-  channel.bind('os-vote', data => {
-    dataPoints = dataPoints.map(x => {
-      if (x.label == data.os) {
-        x.y += data.points
-        return x
-      } else {
-        return x
-      }
-    })
-    chart.render()
-  })
-}
 
